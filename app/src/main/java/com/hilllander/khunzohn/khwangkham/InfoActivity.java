88f7ebc7b173ac918f41a16915b345973f24811c
khunzohn <khunzohn@gmail.com>
2015-10-09 04:07:46 +0630

    add facebook opener

--- app/src/main/java/com/hilllander/khunzohn/khwangkham/InfoActivity.java
+++ app/src/main/java/com/hilllander/khunzohn/khwangkham/InfoActivity.java
@@ -1,15 +1,21 @@
 package com.hilllander.khunzohn.khwangkham;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.hilllander.khunzohn.khwangkham.util.FacebookOpener;
 
 
 public class InfoActivity extends AppCompatActivity {
     private CollapsingToolbarLayout collapse;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,14 @@ public class InfoActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         collapse = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
         collapse.setTitle("Hilllander");
+        fab = (FloatingActionButton) findViewById(R.id.fab_info);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent facebook = FacebookOpener.getOpenFacebookIntent(InfoActivity.this);
+                startActivity(facebook);
+            }
+        });
     }
 
     @Override

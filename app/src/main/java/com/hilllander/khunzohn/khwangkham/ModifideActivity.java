8f976c7213d9fafc89aec25b8a5c1282911a32a1
khunzohn <khunzohn@gmail.com>
2015-10-08 13:29:33 +0630

    removed tabs

--- app/src/main/java/com/hilllander/khunzohn/khwangkham/ModifideActivity.java
+++ app/src/main/java/com/hilllander/khunzohn/khwangkham/ModifideActivity.java
@@ -3,7 +3,6 @@ package com.hilllander.khunzohn.khwangkham;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -11,55 +10,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 public class ModifideActivity extends AppCompatActivity {
-    private static final String POSITION = "position";
 
-    private TabLayout tabLayout;
     private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_modified);
-        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Main"));
-        tabLayout.addTab(tabLayout.newTab().setText("Instruction"));
 
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                switch (position) {
-                    case 0:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.main_frame, ClientFragment.getInstance(position))
-                                .commit();
-                        break;
-                    case 1:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.main_frame, ClientFragment.getInstance(position))
-                                .commit();
-                        break;
-                    default:
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_frame, ClientFragment.getInstance(0))
+                .replace(R.id.main_frame, ClientFragment.getInstance())
                 .commit();
     }
 
@@ -91,19 +54,14 @@ public class ModifideActivity extends AppCompatActivity {
         public ClientFragment() {
         }
 
-        public static Fragment getInstance(int position) {
-            ClientFragment fragment = new ClientFragment();
-            Bundle args = new Bundle();
-            args.putInt(POSITION, position);
-            fragment.setArguments(args);
-            return fragment;
+        public static Fragment getInstance() {
+            return new ClientFragment();
         }
 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.fragment_client, container, false);
-            Button button = (Button) view.findViewById(R.id.button);
             return view;
         }
 
